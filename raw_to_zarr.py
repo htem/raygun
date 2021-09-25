57d0363c43f03c8e4f1ebc4507eda3efdb870834
rhoadesScholar <rhoades@g.harvard.edu>
2021-09-24 20:14:33 -0400

    raw_to_zarr running

--- raw_to_zarr.py
+++ raw_to_zarr.py
@@ -1,35 +1,32 @@
 import numpy as np
 import daisy
 
+
 # --- Parameters and paths --- #
-volume_size = [3200, 3200, 3200]  # In voxels, xyz order
-voxel_size = 120  # In nm
+volume_size = [3216, 3216, 2048]  # In voxels, xyz order
+voxel_size = [30, 30, 30]  # In nm
+total_roi = daisy.Roi((0,0,0), np.array(volume_size)*np.array(voxel_size))
+file_path = '/n/groups/htem/ESRF_id16a/LTP/cb_lobX_sept2020/'
+src_name = 'CBxs_lobV_bottomp100um_30nm_rec_db9_.raw'
+out_name = 'CBxs_lobV_bottomp100um_30nm_rec_db9_.n5'
+ds_name = 'volumes/raw'
+dtype=np.uint8
 
-volume_fn = 's1GadIMSmale_180917_120nm_weakleg_.raw'
-
-
-# --- Create or connect to a cloudvolume --- #
-info = {'filename'=,
-        'ds_name'=,
-        'total_roi'=,
-        'voxel_size'=,
-        'dtype'=,
-        'write_roi'=None,
-        'write_size'=None,
-        'num_channels'=1,
-        'compressor'='default'
-    }
-
-print(f'Opening a cloudvolume at {cloud_path}')
-vol = daisy.prepare_ds(**info)
+print(f'Opening a zarr volume')
+#modified daisy to not have to specify chunk_shape (line 319 in daisy/datasets.py)
+vol = daisy.prepare_ds(file_path+out_name, 
+                       ds_name,
+                       total_roi,
+                        voxel_size,
+                        dtype)
 
 # --- Load image volume and upload --- #
 print('Loading volume into memory...')
-im = np.fromfile(volume_fn, dtype=np.uint8)
+im = np.fromfile(file_path+src_name, dtype=dtype)
 print('Reshaping...')
 im = im.reshape(*volume_size, order='F')
 
-print('Uploading to google cloud...')
-assert vol.shape[:3] == im.shape
-vol[:, :, :] = im
+assert vol.shape == im.shape
+print('Saving to zarr...')
+vol[total_roi] = im
 print('Done.')
