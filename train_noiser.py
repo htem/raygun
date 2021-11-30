e5e35cb4162689d55d8422c939906ee4cfa7f26d
rhoadesScholar <rhoades@g.harvard.edu>
2021-11-29 21:29:03 -0500

    train noiser cost func runs

--- train_noiser.py
+++ train_noiser.py
@@ -3,11 +3,15 @@ print('Importing dependencies...')
 from functools import partial
 import gunpowder as gp
 from numpy.core.numeric import isclose
-from boilerPlate import GaussBlur, Noiser
 from scipy import optimize
+import json
+
+import sys
+
+sys.path.insert(0, '/n/groups/htem/users/jlr54/raygun/')
+from boilerPlate import GaussBlur, Noiser
 
 # Load Trained Discriminator:
-import sys
 sys.path.append('/n/groups/htem/ESRF_id16a/tomo_ML/ResolutionEnhancement/raygun/CycleGAN/')
 from CycleGun_CBv30nmBottom100um_cb2gcl1_20211126_ import *
 
@@ -20,15 +24,15 @@ def bring_the_noise(src, pipeline, noise_order, noise_dict):
         noise_name += noise
         new_array = gp.ArrayKey(noise_name.upper())
         
-        if noise == 'resample' and not isclose(noise_dict[noise]['ratio'], 1):
+        if noise == 'resample':# and not isclose(noise_dict[noise]['ratio'], 1):
             pipeline += gp.Resample(this_array, noise_dict[noise]['base_voxel_size'] * noise_dict[noise]['ratio'], new_array)
-        elif noise == 'gaussBlur' and not isclose(noise_dict[noise], 0):
+        elif noise == 'gaussBlur':# and not isclose(noise_dict[noise], 0):
             pipeline += GaussBlur(this_array, noise_dict[noise], new_array=new_array)
-        elif noise == 'gaussNoise' and not isclose(noise_dict[noise], 0):
+        elif noise == 'gaussNoise':# and not isclose(noise_dict[noise], 0):
             pipeline += Noiser(this_array, new_array=new_array, mode='gaussian', var=noise_dict[noise])
-        elif noise == 'poissNoise' and noise_dict[noise]:
+        elif noise == 'poissNoise':# and noise_dict[noise]:
             pipeline += Noiser(this_array, new_array=new_array, mode='poisson')
-        elif 'noise' in noise and noise_dict[noise]:
+        elif 'noise' in noise:# and noise_dict[noise]:
             pipeline += Noiser(this_array, new_array=new_array, mode=noise_dict[noise]['mode'], **noise_dict[noise]['kwargs'])
         
         noise_name += '_'
@@ -50,13 +54,16 @@ def update_noise_dict(noise_dict, optim_map, optim_vars):
 def cost_func(ref, pre_pipe, post_pipe, critic, out_array, noise_order, noise_dict, optim_map, optim_vars):
     # Setup Noising
     noise_dict = update_noise_dict(noise_dict, optim_map, optim_vars)
-    pipe, _, _ = bring_the_noise(ref.real_B_src, pre_pipe, noise_order, noise_dict)
+    pipe, arrays, noise_name = bring_the_noise(ref.real_B_src, pre_pipe, noise_order, noise_dict)
+    arrays.append(out_array)
     pipe += post_pipe
     
     # Make Batch Request for Training
     request = gp.BatchRequest()
-    extents = ref.get_extents()
-    request.add(out_array, ref.common_voxel_size * extents, ref.common_voxel_size)
+    extents = ref.get_extents(side_length=128)
+    # for array in arrays:
+    #     request.add(array, ref.common_voxel_size * extents)
+    request.add(out_array, ref.common_voxel_size * extents)
 
     # Get Batch
     with gp.build(pipe):
@@ -134,21 +141,8 @@ noise_name = noise_name[:-1]
 out_array = gp.ArrayKey(noise_name.upper() + '_COMMONSIZE')
 post_pipe = gp.Resample(gp.ArrayKey(noise_name.upper()), cycleGun.common_voxel_size, out_array)
 
-# Add augmentations
-# cycleGun.pipe_B += gp.SimpleAugment(mirror_only=augment_axes, transpose_only=augment_axes)
-
-
 post_pipe += gp.Normalize(out_array)
 #TODO: ADD CACHE
-
-# cycleGun.pipe_B += gp.ElasticAugment( #TODO: MAKE THESE SPECS PART OF CONFIG
-#     control_point_spacing=cycleGun.side_length//2,
-#     jitter_sigma=(10.0,)*cycleGun.ndims,
-#     rotation_interval=(0, math.pi/8),
-#     subsample=4,
-#     spatial_dims=cycleGun.ndims
-#     )
-
 
 # add "channel" dimensions if neccessary, else use z dimension as channel
 if cycleGun.ndims == len(cycleGun.common_voxel_size):
@@ -173,7 +167,18 @@ bounds = [
             ]
 options = {'disp': True} 
 
+print('Testing...')
+print(f'Initial loss: {cost_func(cycleGun, pre_pipe, post_pipe, critic, out_array, noise_order, noise_dict, optim_map, optim_vars)}')
+
 print('Optimizing...')
-result = optimize.shgo(func, bounds, options=options)
-print('Done.')
+# result = optimize.shgo(func, bounds, options=options)
+result = optimize.basinhopping(func, optim_vars, niter=1000, disp=True)
 print(f'x = {result.x}, Final loss = {result.fun}, Total # local minima found = {len(result.xl)}')
+
+print('Saving...')
+final_dict = update_noise_dict(noise_dict, optim_map, result.x)
+f = open("EM2XNH_noiseDict.json", "w")
+json.dump(final_dict, f)
+f.close()
+
+print('Done.')
