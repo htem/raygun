ece243a2b968590de405a0614ebfd48925192500
rhoadesScholar <rhoades@g.harvard.edu>
2023-01-24 21:32:29 -0500

    tidying

--- experiments/test_runs/MTLSD/30nm_eroded/retrain.sh
+++
@@ -1,5 +0,0 @@
-rm train.out
-rm -rf tensorboard
-rm -rf models
-rm -rf snapshots
-raygun-train-cluster train_conf.json
--- experiments/test_runs/NotACycleGAN/setup01/retrain.sh
+++
@@ -1,5 +0,0 @@
-rm train.out
-rm -r tensorboard
-rm -r models
-rm -r snapshots
-raygun-train-cluster ./train_conf.json
