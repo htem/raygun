4aedadca85d1477d4438eb0572c115b55d12a15e
rhoadesScholar <rhoades@g.harvard.edu>
2023-01-26 16:44:38 -0500

    tidying

---
+++ experiments/ieee-isbi-2023/01_cycleGAN/render_eval1_netG1.sh
@@ -0,0 +1,6 @@
+raygun-predict /nrs/funke/rhoadesj/raygun/experiments/ieee-isbi-2022/01_cycleGAN/link/seed3/render_eval1_netG1.json
+raygun-predict /nrs/funke/rhoadesj/raygun/experiments/ieee-isbi-2022/01_cycleGAN/link/seed13/render_eval1_netG1.json
+raygun-predict /nrs/funke/rhoadesj/raygun/experiments/ieee-isbi-2022/01_cycleGAN/link/seed42/render_eval1_netG1.json
+raygun-predict /nrs/funke/rhoadesj/raygun/experiments/ieee-isbi-2022/01_cycleGAN/split/seed3/render_eval1_netG1.json
+raygun-predict /nrs/funke/rhoadesj/raygun/experiments/ieee-isbi-2022/01_cycleGAN/split/seed13/render_eval1_netG1.json
+raygun-predict /nrs/funke/rhoadesj/raygun/experiments/ieee-isbi-2022/01_cycleGAN/split/seed42/render_eval1_netG1.json
--- experiments/ieee-isbi-2023/01_cycleGAN_5/retrain.sh
+++
@@ -1,6 +0,0 @@
-rm */*/train.out
-rm -r */*/tensorboard
-rm -r */*/models
-rm -r tensorboards
-raygun-train-batch .
-
