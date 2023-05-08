975b7b149df1bc9853b52b4578a29fc1879b50f2
brianreicher <reicher.b@northeastern.edu>
2023-05-08 13:31:20 -0400

    NLayerDiscriminator docs & type checking

--- src/raygun/torch/networks/NLayerDiscriminator.py
+++ src/raygun/torch/networks/NLayerDiscriminator.py
@@ -3,7 +3,28 @@ import functools
 
 
 class NLayerDiscriminator2D(torch.nn.Module):
-    """Defines a PatchGAN discriminator"""
+    """Defines a 2D PatchGAN discriminator.
+    
+    Args:
+
+        input_nc (``integer``, optional):
+            Number of channels in the input images, with a default of 1.
+            
+        ngf (``integer``, optional):
+            Number of filters in the last convolutional layer, with a default of 64.
+
+        n_layers (``integer``, optional):
+            Number of convolution layers in the discriminator, with a default of 3.
+
+        norm_layer (callable, optional):
+            Normalization layer to use, with the default being 2-dimensional batch normalization.
+
+        kw (``integer``, optional):
+            Kernel size for convolutional layers, with the default being 4.
+            
+        downsampling_kw (optional):
+            Kernel size for downsampling convolutional layers. If not provided, defaults to the same value as kw.
+    """
 
     def __init__(
         self,
@@ -13,33 +34,27 @@ class NLayerDiscriminator2D(torch.nn.Module):
         norm_layer=torch.nn.BatchNorm2d,
         kw=4,
         downsampling_kw=None,
-    ):
-        """Construct a PatchGAN discriminator
-        Parameters:
-            input_nc (int)  -- the number of channels in input images
-            ngf (int)       -- the number of filters in the last conv layer
-            n_layers (int)  -- the number of conv layers in the discriminator
-            norm_layer      -- normalization layer
-        """
+    ) -> None:
+
         super().__init__()
         if (
             type(norm_layer) == functools.partial
         ):  # no need to use bias as BatchNorm2d has affine parameters
-            use_bias = norm_layer.func == torch.nn.InstanceNorm2d
+            use_bias: bool = norm_layer.func == torch.nn.InstanceNorm2d
         else:
-            use_bias = norm_layer == torch.nn.InstanceNorm2d
+            use_bias: bool = norm_layer == torch.nn.InstanceNorm2d
 
         if downsampling_kw is None:
-            downsampling_kw = kw
+            downsampling_kw: int = kw
 
-        padw = 1
-        ds_kw = downsampling_kw
-        sequence = [
+        padw:int = 1
+        ds_kw:int = downsampling_kw
+        sequence: list = [
             torch.nn.Conv2d(input_nc, ngf, kernel_size=ds_kw, stride=2, padding=padw),
             torch.nn.LeakyReLU(0.2, True),
         ]
-        nf_mult = 1
-        nf_mult_prev = 1
+        nf_mult: int = 1
+        nf_mult_prev: int = 1
         for n in range(1, n_layers):  # gradually increase the number of filters
             nf_mult_prev = nf_mult
             nf_mult = min(2**n, 8)
@@ -74,36 +89,61 @@ class NLayerDiscriminator2D(torch.nn.Module):
         sequence += [
             torch.nn.Conv2d(ngf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)
         ]  # output 1 channel prediction map
-        self.model = torch.nn.Sequential(*sequence)
+        self.model: torch.nn.Sequential = torch.nn.Sequential(*sequence)
 
     @property
-    def FOV(self):
+    def FOV(self) -> float:
         # Returns the receptive field of one output neuron for a network (written for patch discriminators)
         # See https://distill.pub/2019/computing-receptive-fields/#solving-receptive-field-region for formula derivation
 
-        L = 0  # num of layers
-        k = []  # [kernel width at layer l]
-        s = []  # [stride at layer i]
+        L: int = 0  # num of layers
+        k: list = []  # [kernel width at layer l]
+        s: list = []  # [stride at layer i]
         for layer in self.model:
             if hasattr(layer, "kernel_size"):
                 L += 1
                 k += [layer.kernel_size[-1]]
                 s += [layer.stride[-1]]
 
-        r = 1
+        r: float = 1.
         for l in range(L - 1, 0, -1):
-            r = s[l] * r + (k[l] - s[l])
+            r: float = s[l] * r + (k[l] - s[l])
 
         return r
 
-    def forward(self, input):
-        """Standard forward."""
+    def forward(self, input:torch.Tensor) -> torch.Tensor:
+        """Standard forward discriminator pass.
+        
+        Args:
+
+            input (``torch.Tensor``):
+                Image tensor to pass through the network.
+        """
         return self.model(input)
 
 
 class NLayerDiscriminator3D(torch.nn.Module):
-    """Defines a PatchGAN discriminator"""
+    """Defines a 3D PatchGAN discriminator.
+        
+        Args:
+            input_nc (``integer``, optional):
+                Number of channels in the input images, with a default of 1.
+                
+            ngf (``integer``, optional):
+                Number of filters in the last convolutional layer, with a default of 64.
 
+            n_layers (``integer``, optional):
+                Number of convolution layers in the discriminator, with a default of 3.
+
+            norm_layer (callable, optional):
+                Normalization layer to use, with the default being 3-dimensional batch normalization.
+
+            kw (``integer``, optional):
+                Kernel size for convolutional layers, with the default being 4.
+                
+            downsampling_kw (optional):
+                Kernel size for downsampling convolutional layers. If not provided, defaults to the same value as kw.
+    """
     def __init__(
         self,
         input_nc=1,
@@ -112,33 +152,27 @@ class NLayerDiscriminator3D(torch.nn.Module):
         norm_layer=torch.nn.BatchNorm3d,
         kw=4,
         downsampling_kw=None,
-    ):
-        """Construct a PatchGAN discriminator
-        Parameters:
-            input_nc (int)  -- the number of channels in input images
-            ngf (int)       -- the number of filters in the last conv layer
-            n_layers (int)  -- the number of conv layers in the discriminator
-            norm_layer      -- normalization layer
-        """
+    ) -> None:
+
         super().__init__()
         if (
             type(norm_layer) == functools.partial
         ):  # no need to use bias as BatchNorm3d has affine parameters
-            use_bias = norm_layer.func == torch.nn.InstanceNorm3d
+            use_bias: bool = norm_layer.func == torch.nn.InstanceNorm3d
         else:
-            use_bias = norm_layer == torch.nn.InstanceNorm3d
+            use_bias: bool = norm_layer == torch.nn.InstanceNorm3d
 
         if downsampling_kw is None:
-            downsampling_kw = kw
+            downsampling_kw: int = kw
 
-        padw = 1
-        ds_kw = downsampling_kw
-        sequence = [
+        padw: int = 1
+        ds_kw: int = downsampling_kw
+        sequence: list = [
             torch.nn.Conv3d(input_nc, ngf, kernel_size=ds_kw, stride=2, padding=padw),
             torch.nn.LeakyReLU(0.2, True),
         ]
-        nf_mult = 1
-        nf_mult_prev = 1
+        nf_mult: int = 1
+        nf_mult_prev: int = 1
         for n in range(1, n_layers):  # gradually increase the number of filters
             nf_mult_prev = nf_mult
             nf_mult = min(2**n, 8)
@@ -173,24 +207,33 @@ class NLayerDiscriminator3D(torch.nn.Module):
         sequence += [
             torch.nn.Conv3d(ngf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)
         ]  # output 1 channel prediction map
-        self.model = torch.nn.Sequential(*sequence)
+        self.model: torch.nn.Sequential = torch.nn.Sequential(*sequence)
 
-    def forward(self, input):
-        """Standard forward."""
+    def forward(self, input:torch.Tensor) -> torch.Tensor:
+        """Standard forward discriminator pass.
+        
+        Args:
+
+            input (``torch.Tensor``):
+                Image tensor to pass through the network.
+        """
+
         return self.model(input)
 
 
 class NLayerDiscriminator(NLayerDiscriminator2D, NLayerDiscriminator3D):
-    """Defines a PatchGAN discriminator"""
+    """Interface for a PatchGAN discriminator.
+  
+        Args:
+            ndims (``integer``):
+                Number of image dimensions, to create a 2D or 3D PatchGAN discriminator.
 
-    def __init__(self, ndims, **kwargs):
-        """Construct a PatchGAN discriminator
-        Parameters:
-            input_nc (int)  -- the number of channels in input images
-            ngf (int)       -- the number of filters in the last conv layer
-            n_layers (int)  -- the number of conv layers in the discriminator
-            norm_layer      -- normalization layer
-        """
+            **kwargs:
+                Optional keyword arguments defining network hyper parameters.     
+    """
+
+    def __init__(self, ndims:int, **kwargs) -> None:
+
         if ndims == 2:
             NLayerDiscriminator2D.__init__(self, **kwargs)
         elif ndims == 3:
