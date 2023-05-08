d644e99726fb2e79e5f2523c0dbf870e3340c7e6
brianreicher <reicher.b@northeastern.edu>
2023-05-08 15:25:43 -0400

    Optimizer utils docs and type checking

--- src/raygun/torch/optimizers/utils.py
+++ src/raygun/torch/optimizers/utils.py
@@ -1,9 +1,20 @@
 import torch
 
 
-def get_base_optimizer(optim):
+def get_base_optimizer(optim) -> torch.optim.Optimizer:
+    """Return the base optimizer object given its name or object.
+
+    Args:
+        optim (``string``, torch.optim.Optimizer``): 
+            String or optimizer object
+
+    Returns:
+        ``torch.optim.Optimizer``:
+            The base optimizer object.
+    """
+
     if isinstance(optim, str):
-        base_optim = getattr(torch.optim, optim)
+        base_optim: torch.optim.Optimizer = getattr(torch.optim, optim)
     else:
         base_optim = optim
     return base_optim
